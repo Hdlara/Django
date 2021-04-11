105ac6f16d667ab7a8f8afc1004341e77fadfb5b
henrique-vedois <75740946+henrique-vedois@users.noreply.github.com>
2021-04-11 17:28:48 -0300

    configuring the Django admin

--- core/models.py
+++ core/models.py
@@ -6,8 +6,13 @@ class Product(models.Model):
     price = models.DecimalField('Price', decimal_places=2, max_digits=8)
     stock = models.IntegerField('Quantity in Stock')
 
+    def __str__(self):
+        return self.name
 
 class Client(models.Model):
     name = models.CharField('Name', max_length=100)
     surname = models.CharField('Surname', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
+
+    def __str__(self):
+        return self.name
