6b606097d0b700c822a02dba2d2b77aa354ea6ec
henrique-vedois <75740946+henrique-vedois@users.noreply.github.com>
2021-04-12 21:04:48 -0300

    working with templates

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from.views import index, contato
+from.views import index, contato, product
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('contato', contato),
+    path('product/<int:id>', product, name='product'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+from .models import Product
 
 def index(request):
+    products = Product.objects.all()
+
     context = {
         'curso': 'Programação web com Django Framework',
-        'outros': 'Jango é massa'
+        'outros': 'Jango é massa',
+        'products': products
     }
     return render(request, 'index.html', context)
 
@@ -12,3 +16,9 @@ def index(request):
 def contato(request):
     return render(request, 'contato.html')
 
+def product(request, id):
+    prod = Product.objects.get(id=id)
+    context = {
+        'product': prod
+    }
+    return render(request, 'product.html', context)
