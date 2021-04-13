efa96cf97d6472d31a3727a0ff1ae4d61a85f756
henrique-vedois <75740946+henrique-vedois@users.noreply.github.com>
2021-04-13 19:26:56 -0300

    creating templates with js and css and exception handling

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Product
+
+from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     products = Product.objects.all()
@@ -17,8 +21,18 @@ def contato(request):
     return render(request, 'contato.html')
 
 def product(request, id):
-    prod = Product.objects.get(id=id)
+    #prod = Product.objects.get(id=id)
+    prod = get_object_or_404(Product, id=id)
     context = {
         'product': prod
     }
     return render(request, 'product.html', context)
+
+def error404(request, ex):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=500)
--- django1/settings.py
+++ django1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '28+n&qtri@(0%w297p+gaq15g_-kql7si0g&*^^lqfb$a+-=$%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# debug True = modo desenvolvimento / debug False = modo producao
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -118,4 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'   # usado durante o desenvolvimento
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # usado durante a produção
+
+LOGOUT_REDIRECT_URL = 'index'
